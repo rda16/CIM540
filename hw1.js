e665ec738e7c112ba98887617963b2b4851a2c18
CIM540 <Dandrik@Daniels-MacBook-Pro-2.local>
2017-02-09 12:41:15 -0500

    Week2 Practice
    Its a pumpkin

---
+++ hw1.js
@@ -0,0 +1,96 @@
+var mouthX = 25;
+var mouthY = 250;
+var mouthW = 400;
+var mouthH = 60;
+
+var eye1X = 70;
+var eye1Y = 70;
+
+var eye2offset = 300;
+var pupilOffset = 20;
+
+var backgroundColor = 'orange';
+
+var randNum = 0;
+
+var mapX = 0;
+
+function setup() {
+    createCanvas(500, 400);
+    background(0, 0, 0);
+    background(255, 0, 0);
+    background(125);
+    
+    console.log("width: " + width + " height: " + height);
+    
+    //noStroke();  //removes lines
+    //randNum = random(255); //range from 0-254
+    //console.log(randNum);
+}
+
+function draw() {
+    frameRate(30);
+    background(backgroundColor);
+    
+    //mapX = map(mouseX, 0, width, -10, 10);
+    
+    //fill(255);
+    //eye1
+    //(x,y,width,height)
+    triangle(300,140, 356, 20, 412, 140);    
+    //fill(0,0,255);
+    //fill(0,0,255);
+    //ellipse(eye1X + mapX, eye1Y + pupilOffset ,25,50);//pupil
+    
+    //eye2
+    //fill(255);
+    triangle(60, 140, 116, 20, 172, 140);    
+    //fill(0,0,255);
+    //ellipse(eye1X + mapX, eye1Y + pupilOffset,25,50);//pupil
+    
+    fill(250);
+    //nose
+    triangle(width/2, height/2, (width/2) + 20, (height/2) + 20, (width/2) - 20, (height/2) + 20);   
+    
+    fill(255);         
+    //mouth
+    //rect(mouthX, mouthY, mouthW, mouthH);
+    //stroke(125);
+    
+    arc(245, 275, 300, 150, 0, PI);
+    strokeWeight(3);
+    rect(150, 276, 55/2, 55/2);
+    rect(300, 276, 55/2, 55/2);
+
+    strokeWeight(7);
+    
+    arc(60, 150, 110, 350, 90, PI, closed);
+    arc(525, 150, 110, 1000, 90, PI, closed);
+    //trouble finding a way to create the grooves of the pumkin
+
+
+    
+    //ellipse(mouseX, mouseY, 10, 10);
+
+    //noStroke();
+    
+    //toungue
+    //fill('red')
+
+    //arc(width/2 + 100, 275, 100, 100, 0, PI);
+    
+    //ellipse(mouseX, mouseY, 10, 10);
+
+}
+
+
+
+//function mousePressed(){
+    //backgroundColor= randNum = random(255); //range from 0-254;
+    //console.log(randNum);
+//}
+
+//function mouseReleased(){
+   // backgroundColor= 'blue';
+//}
+
